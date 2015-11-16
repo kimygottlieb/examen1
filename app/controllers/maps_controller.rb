0cba915061f06eec1723eb1cd583ac2c4fdb3ced
gabo <kimy.gl@hotmail.com>
2015-11-15 22:20:22 -0600

    mapa

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,15 +1,9 @@
 class MapsController < ApplicationController
   def index
-	@map = GMaps.new(div: '#map', lat: -12.043333, lng: -77.028333)
-	@map.addMarker(lat: -12.043333,
-               lng: -77.028333,
-               title: 'Lima',
+	@map = GMaps.new(div: '#map', lat: 15.5, lng: -88.0333)
+	@map.addMarker(lat: 15.5,
+               lng: -88.0333,
+               title: 'SPS',
                click: GMaps::JS["function(e) { alert('You clicked in this marker'); }"])
-	@map.addMarker(lat: -12.042,
-               lng: -77.028333,
-               title: 'Marker with InfoWindow',
-               infoWindow: {
-                 content: '<p>HTML Content</p>'
-               })
   end
 end
